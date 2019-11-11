384c66f0447609fefd7c8eb7c9bf0284a6d1feeb
EvanKal <kalabokisevangelos@gmail.com>
2019-11-11 12:26:48 +0200

    Large coupon

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
   initiateIconCarousel();
   modifyCarouselsToSingleSlideCarousel();
   checkIfCardDescriptionOverflows();
+  fillStarsInCards(3);
 
   $('.carousel').carousel({
     touch: true,
@@ -65,7 +66,7 @@ window.addEventListener('DOMContentLoaded', () => {
       ]
     });
 
-    
+
 
     $('.mySlickCarousel.large-coupon-carousel').slick({
       infinite: true,
@@ -246,13 +247,44 @@ function nonSlidingCarousel() {
 function checkIfCardDescriptionOverflows() {
   let blockswithtext = $('.card p');
 
-  blockswithtext.each(function(){
+  blockswithtext.each(function () {
     let elem = $(this);
     console.log(elem.parent().innerHeight());
     console.log(elem.prop("scrollHeight"));
 
-    if(elem.parent().innerHeight() < elem.innerHeight()) {
+    if (elem.parent().innerHeight() < elem.innerHeight()) {
       elem.addClass("block-with-text");
     }
+  });
+}
+
+//Fill star rating display
+
+function fillStarsInCards(rating) {
+
+  //Temporary method to fill all the rating displays with stars based on a variable
+
+  let allcards = document.querySelectorAll(".card .rating-display");
+
+  allcards.forEach((elem) => {
+
+    //The actual method body to be used later
+
+    let fragment = document.createDocumentFragment();
+
+    for (i = 1; i <= 5; i++) {
+      let star = document.createElement("span");
+
+      if (i <= rating) {
+        star.setAttribute("class", "fa fa-star pr-1");
+      } else {
+        star.setAttribute("class", "fa fa-star-o pr-1");
+      }
+
+      fragment.appendChild(star);
+    }
+
+    elem.appendChild(fragment);
+
   });
 }
