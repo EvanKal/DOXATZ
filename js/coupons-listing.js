c13332a4ae2f43488f22d7b8f11ecb57e91bf1ec
EvanKal <kalabokisevangelos@gmail.com>
2019-11-25 10:50:44 +0200

    arrow toggling for the accordion button

--- js/coupons-listing.js
+++ js/coupons-listing.js
@@ -1,36 +1,55 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    // togglecategoriesDropdownMenuArrow();
+    togglecategoriesDropdownMenuArrow();
 
 });
 
 
+function togglecategoriesDropdownMenuArrow() {
+    $("#accordionDropdownMenuContainer").on('hide.bs.collapse', function (event) {
 
+        console.log("hidden");
+
+        if ($('#categoriesAccordionButton .iconcontainer i').hasClass("menulinkiconrotate")) {
+            $('#categoriesAccordionButton .iconcontainer i').removeClass("menulinkiconrotate");
+        }
+    });
+
+
+    $("#accordionDropdownMenuContainer").on('show.bs.collapse', function (event) {
+
+        console.log("Shown");
+
+        if (!$('#categoriesAccordionButton .iconcontainer i').hasClass("menulinkiconrotate")) {
+            $('#categoriesAccordionButton .iconcontainer i').addClass("menulinkiconrotate");
+        }
+    });
+}
 
 
 
 
 // Not used
 
-function togglecategoriesAccordionDropdown() {
+// function togglecategoriesAccordionDropdown() {
 
-    document.querySelector("#categoriesAccordion").addEventListener("click", function() {
+//     document.querySelector("#categoriesAccordion").addEventListener("click", function() {
         
-    });
-}
+//     });
+// }
 
-function togglecategoriesDropdownMenuArrow {
-    $("#categoriesDropdownMenu").on('hide.bs.dropdown', function (event) {
+// function togglecategoriesDropdownMenuArrow() {
+//     $("#categoriesDropdownMenu").on('hide.bs.dropdown', function (event) {
 
-        if ($('#categoriesDropdownMenu .iconcontainer i').hasClass("menulinkiconrotate")) {
-            $('#categoriesDropdownMenu .iconcontainer i').removeClass("menulinkiconrotate");
-        }
-    });
+//         if ($('#categoriesDropdownMenu .iconcontainer i').hasClass("menulinkiconrotate")) {
+//             $('#categoriesDropdownMenu .iconcontainer i').removeClass("menulinkiconrotate");
+//         }
+//     });
 
 
-    $("#categoriesDropdownMenu").on('show.bs.dropdown', function (event) {
-        if (!$('#categoriesDropdownMenu .iconcontainer i').hasClass("menulinkiconrotate")) {
-            $('#categoriesDropdownMenu .iconcontainer i').addClass("menulinkiconrotate");
-        }
-    });
-}
+//     $("#categoriesDropdownMenu").on('show.bs.dropdown', function (event) {
+//         if (!$('#categoriesDropdownMenu .iconcontainer i').hasClass("menulinkiconrotate")) {
+//             $('#categoriesDropdownMenu .iconcontainer i').addClass("menulinkiconrotate");
+//         }
+//     });
+// }
